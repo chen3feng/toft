acba23e6b2d7cac85ab9562b0e6efdd6d1ffa8c8
chen3feng <chen3feng@gmail.com>
2013-05-08 22:12:25 +0800

    Add binary_version and static_resource

---
+++ base/binary_version_test.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2011, The Toft Authors.
+// All rights reserved.
+//
+// Author: CHEN Feng <chen3feng@gmail.com>
+// Created: 05/05/11
+// Description: test binary version
+
+#include "toft/base/binary_version.h"
+#include "thirdparty/gtest/gtest.h"
+
+TEST(BinaryVersion, Test)
+{
+    EXPECT_STRNE("Unknown", binary_version::kBuilderName);
+    EXPECT_STRNE("Unknown", binary_version::kBuildTime);
+    EXPECT_STRNE("Unknown", binary_version::kHostName);
+    EXPECT_STRNE("Unknown", binary_version::kCompiler);
+}
