184749266d98c74bd0d17de7f3c8f4c322a5a73d
yeshunping <yeshunping@gmail.com>
2013-05-19 00:16:15 +0800

    refactor hash code, write benchmark for them

---
+++ hash/hash_benchmark.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2013, The Toft Authors. All rights reserved.
+// Author: Ye Shunping <yeshunping@gmail.com>
+
+#include "toft/hash/hash.h"
+#include "toft/base/benchmark.h"
+
+const std::string test_str = "qwertyuiopasdfghjklmnbvcz";
+
+static void Fingerprint(int n) {
+    for (int i = 0; i < n; i++) {
+        toft::Fingerprint(test_str);
+    }
+}
+
+static void Fingerprint32(int n) {
+    for (int i = 0; i < n; i++) {
+        toft::Fingerprint32(test_str);
+    }
+}
+
+static void JenkinsOneAtATimeHash(int n) {
+    for (int i = 0; i < n; i++) {
+        toft::JenkinsOneAtATimeHash(test_str);
+    }
+}
+
+static void SuperFastHash(int n) {
+    for (int i = 0; i < n; i++) {
+        toft::SuperFastHash(test_str);
+    }
+}
+BENCHMARK(Fingerprint)->ThreadRange(1, NumCPUs());
+BENCHMARK(Fingerprint32)->ThreadRange(1, NumCPUs());
+BENCHMARK(JenkinsOneAtATimeHash)->ThreadRange(1, NumCPUs());
+BENCHMARK(SuperFastHash)->ThreadRange(1, NumCPUs());
+
