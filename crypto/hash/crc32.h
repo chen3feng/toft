cbaa8bd82e77701607ef3e0fdd29ab2b6f4ae8ce
yeshunping <yeshunping@gmail.com>
2013-06-12 13:38:17 +0800

    add crc32

--- crypto/hash/crc32.h
+++ crypto/hash/crc32.h
@@ -3,8 +3,8 @@
 //
 // Author: Ye Shunping <yeshunping@gmail.com>
 
-#ifndef TOFT_CRYPTO_HASH_SHA1_H
-#define TOFT_CRYPTO_HASH_SHA1_H
+#ifndef TOFT_CRYPTO_HASH_CRC32_H
+#define TOFT_CRYPTO_HASH_CRC32_H
 
 #include <stdint.h>
 #include <string>
@@ -14,40 +14,33 @@
 
 namespace toft {
 
-class SHA1 {
-    TOFT_DECLARE_UNCOPYABLE(SHA1);
+class CRC32 {
+    TOFT_DECLARE_UNCOPYABLE(CRC32);
 
 public:
-    SHA1();
-    ~SHA1();
+    CRC32();
+    ~CRC32();
 
     //  Init is called in constructor, but if you want to use the same object
     //  for many times, you SHOULD call Init before computing sha1 of new data.
     void Init();
     void Update(StringPiece sp);
-    // Finalizes the Sha1 operation and fills the buffer with the digest.
-    //  Data is uint8_t digest_[20]
+    uint32_t Final();
+    // Finalizes the MD5 operation and fills the buffer with the digest.
+    //  Data is uint32_t_t
     void Final(void* digest);
     //  Hex encoding for result
     std::string HexFinal();
 
+    static uint32_t Digest(StringPiece sp);
     static std::string HexDigest(StringPiece sp);
 
 private:
-    void SHA1Transform(uint32_t state[5], const uint8_t buffer[64]);
-    void Update(const uint8_t* data, size_t input_len);
-    void FinalInternal();
+    void InitCrc32Table();
 
-private:
-    struct SHA1_CTX {
-        uint32_t state[5];
-        uint32_t count[2];  // Bit count of input.
-        uint8_t buffer[64];
-    };
-
-    SHA1_CTX context_;
-    uint8_t digest_[20];
+    uint32_t result_;
+    uint32_t crc32_table_[256];
 };
 
 }  // namespace toft
-#endif  // TOFT_CRYPTO_HASH_SHA1_H
+#endif  // TOFT_CRYPTO_HASH_CRC32_H
